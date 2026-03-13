ea4614d7a71f8b9be342b274a886b8e004626153
VictorMs <martinssantosvictor@gmail.com>
2026-03-13 20:56:48 -0300

    adicionei a primeira parte do sistema do banco. aonde dá as boas vindas e pergunta o nome para o usuario

--- Sistema Banco/src/Main.java
+++ Sistema Banco/src/Main.java
@@ -1,7 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
 
+        double saldoConta = 1000.00;
+        int opcao = 0;
+
+        System.out.println("Olá, Bem vindo ao Banco Digital!");
+        System.out.println("----------------------------------");
+        System.out.println("Digite Seu Nome: ");
+        String nomeUsuario = scanner.nextLine();
+
+        switch (opcao){
+            case 1:
+        }
+
+        System.out.println(saldoConta + nomeUsuario);
 
     }
 }
