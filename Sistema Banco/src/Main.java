b21204da72087e51227dd84c7fc75cbac8956798
VictorMs <martinssantosvictor@gmail.com>
2026-03-13 22:14:06 -0300

    aqui eu adicionei as opções juntamente com laço de repetiçções e condicionais.

--- Sistema Banco/src/Main.java
+++ Sistema Banco/src/Main.java
@@ -13,11 +13,46 @@ public class Main {
         System.out.println("Digite Seu Nome: ");
         String nomeUsuario = scanner.nextLine();
 
-        switch (opcao){
-            case 1:
+
+        while (opcao != 4){
+            System.out.println(nomeUsuario + " Escolhe uma dessas opções abaixo: ");
+            System.out.println("1 - Consultar Saldo ");
+            System.out.println("2 - Depositar");
+            System.out.println("3 - Sacar");
+            System.out.println("4 - Sair");
+            opcao = scanner.nextInt();
+
+            switch (opcao){
+                case 1:
+                    System.out.println(nomeUsuario + " Seu Saldo atual é: " + saldoConta);
+                    break;
+                case 2:
+                    System.out.println("Qual Valor você deseja depositar?  ");
+                    double depositoUsuario = scanner.nextInt();
+                    if (depositoUsuario > 0){
+                        saldoConta = depositoUsuario + saldoConta;
+                        System.out.println("Foi adicionado " + depositoUsuario + " ao seu Saldo da conta.");
+                    }else {
+                        System.out.println("Valor Incorreto!, Tente um valor mais alto!");
+                        break;
+                    }
+                case 3:
+                    System.out.println("Informe o valor do saque: ");
+                    double sacar = scanner.nextDouble();
+                    if (sacar > 0){
+                        saldoConta = saldoConta - sacar;
+                    }else if (sacar > saldoConta){
+                        System.out.println("Valor incorreto, o número saque maior que o saldo atual");
+                    }else {
+                        System.out.println("Valor incorreto, tente um valor mais alto!");
+                    }
+
+            }
         }
 
-        System.out.println(saldoConta + nomeUsuario);
+
+
+
 
     }
 }
